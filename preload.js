68855a59702645cd9bd9a3d30dca091c2c13b0c5
KR0A1TY <medal.fever@gmail.com>
2019-11-06 16:55:17 +0800

    init

---
+++ preload.js
@@ -0,0 +1,51 @@
+const { readFileSync } = require('fs')
+
+
+
+readjson = function () {
+
+    const if_data  = JSON.parse(readFileSync("/Users/kroaity/Documents/utlools/IFS/assets/if.json"));
+    return if_data;
+}
+
+levenshtein = function(a, b) {
+    if(a.length == 0) return b.length; 
+    if(b.length == 0) return a.length;
+  
+    // swap to save some memory O(min(a,b)) instead of O(a)
+    if(a.length > b.length) {
+      var tmp = a;
+      a = b;
+      b = tmp;
+    }
+  
+    var row = [];
+    // init the row
+    for(var i = 0; i <= a.length; i++){
+      row[i] = i;
+    }
+  
+    // fill in the rest
+    for(var i = 1; i <= b.length; i++){
+      var prev = i;
+      for(var j = 1; j <= a.length; j++){
+        var val;
+        if(b.charAt(i-1) == a.charAt(j-1)){
+          val = row[j-1]; // match
+        } else {
+          val = Math.min(row[j-1] + 1, // substitution
+                         prev + 1,     // insertion
+                         row[j] + 1);  // deletion
+        }
+        row[j - 1] = prev;
+        prev = val;
+      }
+      row[a.length] = prev;
+    }
+  
+    return row[a.length];
+  }
+
+function comparef(x, y) {
+    return levenshtein(x, "nature") - levenshtein(y, "nature")
+}
